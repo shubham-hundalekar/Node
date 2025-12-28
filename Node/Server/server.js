6fc71afce324b3ffefb09f544aca7504c7f6f663
shubham <shubhamhundalekar62668@gmail.com>
2025-12-28 21:35:13 +0530

    Server folder added

---
+++ Node/Server/server.js
@@ -0,0 +1,9 @@
+const http = require("http");
+const server = http.createServer((req, res)=>{
+    res.end("Hello world ");
+});
+
+server.listen(7777, (req, res)=>{
+    console.log(`Listening on PORT `+7777)
+});
+
